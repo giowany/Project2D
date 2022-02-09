b5515319f4ba24f10250c33821a233934902c4e3
Giowany Ribeiro <giowany_100@hotmail.com>
2022-02-08 21:29:18 -0300

    Update nas animações do MenuButtonsManager

--- Assets/Scripts/Menu/MenuButtonsManager.cs
+++ Assets/Scripts/Menu/MenuButtonsManager.cs
@@ -1,8 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class MenuButtonsManager : MonoBehaviour
 {
+    public List<GameObject> buttons;
+    [Header("Animation Setings")]
+    public float duration = .5f;
+    public float delay = .5f;
+    public Ease ease;
 
+    private void OnEnable()
+    {
+        AnimationButtons();
+    }
+
+    private void Awake()
+    {
+        HideButtons();
+    }
+
+    private void HideButtons()
+    {
+        foreach (var button in buttons)
+        {
+            button.SetActive(false);
+            button.transform.localScale = Vector3.zero;
+        }
+    }
+
+    private void AnimationButtons()
+    {
+        for (int i = 0; i < buttons.Count; i++)
+        {
+            var button = buttons[i];
+            button.SetActive(true);
+            button.transform.DOScale(1, duration).SetEase(ease).SetDelay(i * delay);
+        }
+    }
 }
