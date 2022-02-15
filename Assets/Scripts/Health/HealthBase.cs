1914e843d413440147cbe33d8e5ffc63d6d92052
Giowany Ribeiro <giowany_100@hotmail.com>
2022-02-15 20:39:20 -0300

    implementado as animaçoes no codigo

--- Assets/Scripts/Health/HealthBase.cs
+++ Assets/Scripts/Health/HealthBase.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class HealthBase : MonoBehaviour
 {
+    [Header("References")]
+    public Animator playeranimator;
+
     public int startLife = 10;
     public float timeToDie = 1f;
+    public string healthFloat = "Health";
+    public string deadBool = "Dead";
 
     private int _currenteLife;
     private bool _isDead = false;
@@ -19,17 +24,31 @@ public class HealthBase : MonoBehaviour
         if (_currenteLife <= 0)
         {
             Kill();
+            Invoke(nameof(SetDead), .1f);
         }
+    }
+
+    private void SetDead()
+    {
+        playeranimator.SetBool(deadBool, true);
     }
 
     private void Kill()
     {
         _isDead = true;
         Destroy(gameObject, timeToDie);
+        playeranimator.SetFloat(healthFloat, _currenteLife);
+    }
+
+    private void Init()
+    {
+        _currenteLife = startLife;
+        playeranimator.SetFloat(healthFloat, _currenteLife);
+        playeranimator.SetBool(deadBool, false);
     }
 
     private void Awake()
     {
-        _currenteLife = startLife;
+        Init();
     }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,13 +8,22 @@ public class PlayerController : MonoBehaviour
 {
     [Header("References")]
     public Rigidbody2D playerRigidBody;
-    public Image imageplayer;
+    public Animator animatorPlayer;
 
     [Header("Moviment Setup")]
     public float speed;
     public float speedRun;
     public float jumpForce;
     public Vector2 friction = new Vector2(.1f, 0);
+
+    [Header("Animations Setup")]
+    public string runBool = "Run";
+    public float speedWalkAnim = 1f;
+    public float speedRunAnim = 1.5f;
+    public float swapDuration = .3f;
+    public string jumpBool = "Jump";
+    public string landingBool = "Landing";
+
 
     [Header("Animation Jump Setup")]
     public float jumpScaleY = 1.5f;
@@ -32,10 +41,28 @@ public class PlayerController : MonoBehaviour
 
     private void HandleMoviment()
     {
-        if (!Input.GetButton("Run"))
+        if (!Input.GetButton("Run") && Input.GetButton("Horizontal"))
+        {
             playerRigidBody.velocity = new Vector2(Input.GetAxis("Horizontal") * speed * Time.deltaTime * 100, playerRigidBody.velocity.y);
-        else if (Input.GetButton("Run"))    
+            if (Input.GetAxis("Horizontal") > 0) playerRigidBody.transform.DOScaleX(1, swapDuration);
+            else if (Input.GetAxis("Horizontal") < 0) playerRigidBody.transform.DOScaleX(-1, swapDuration);
+            animatorPlayer.SetBool(runBool, true);
+            animatorPlayer.speed = speedWalkAnim;
+        }
+        else if (Input.GetButton("Run") && Input.GetButton("Horizontal"))
+        {
             playerRigidBody.velocity = new Vector2(Input.GetAxis("Horizontal") * speedRun * Time.deltaTime * 100, playerRigidBody.velocity.y);
+            if (Input.GetAxis("Horizontal") > 0) playerRigidBody.transform.DOScaleX(1, swapDuration);
+            else if (Input.GetAxis("Horizontal") < 0) playerRigidBody.transform.DOScaleX(-1, swapDuration);
+            animatorPlayer.SetBool(runBool, true);
+            animatorPlayer.speed = speedRunAnim;
+        }
+
+        else
+        {
+            animatorPlayer.SetBool(runBool, false);
+            animatorPlayer.speed = speedWalkAnim;
+        }
 
         if(playerRigidBody.velocity.x < 0)
         {
@@ -54,17 +81,29 @@ public class PlayerController : MonoBehaviour
         if (Input.GetButtonDown("Jump"))
         {
             playerRigidBody.velocity = Vector2.up * jumpForce;
-            playerRigidBody.transform.localScale = Vector2.one;
+            if (playerRigidBody.transform.localScale.x > 0) playerRigidBody.transform.localScale = Vector2.one;
+            else if (playerRigidBody.transform.localScale.x < 0) playerRigidBody.transform.localScale = new Vector2(-1, 1);
             DOTween.Kill(playerRigidBody.transform);
             HandleAnimationJump();
+            animatorPlayer.SetBool(jumpBool, true);
             _jumping = true;
         }
     }
 
     private void HandleAnimationJump()
     {
-        playerRigidBody.transform.DOScaleY(jumpScaleY, durationAnimation).SetLoops(2, LoopType.Yoyo).SetEase(ease);
-        playerRigidBody.transform.DOScaleX(jumpScaleX, durationAnimation).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+        if (playerRigidBody.transform.localScale.x > 0)
+        {
+            playerRigidBody.transform.DOScaleY(jumpScaleY, durationAnimation).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+            playerRigidBody.transform.DOScaleX(jumpScaleX, durationAnimation).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+
+        }
+        else if (playerRigidBody.transform.localScale.x < 0)
+        {
+            playerRigidBody.transform.DOScaleY(jumpScaleY, durationAnimation).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+            playerRigidBody.transform.DOScaleX(-jumpScaleX, durationAnimation).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+
+        }
         _Anim = false;
     }
 
@@ -74,17 +113,36 @@ public class PlayerController : MonoBehaviour
 
         if (!_Anim)
         {
-            playerRigidBody.transform.localScale = Vector2.one;
+            animatorPlayer.SetBool(landingBool, true);
+            animatorPlayer.SetBool(jumpBool, false);
+            if (playerRigidBody.transform.localScale.x > 0) playerRigidBody.transform.localScale = Vector2.one;
+            else if (playerRigidBody.transform.localScale.x < 0) playerRigidBody.transform.localScale = new Vector2(-1, 1);
             DOTween.Kill(playerRigidBody.transform);
-            playerRigidBody.transform.DOScaleX(fallScaleX, durationAnimFall).SetLoops(2, LoopType.Yoyo).SetEase(ease);
-            playerRigidBody.transform.DOScaleY(fallScaleY, durationAnimFall).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+            if (playerRigidBody.transform.localScale.x > 0)
+            {
+                playerRigidBody.transform.DOScaleX(fallScaleX, durationAnimFall).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+                playerRigidBody.transform.DOScaleY(fallScaleY, durationAnimFall).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+
+            }
+            else if (playerRigidBody.transform.localScale.x < 0)
+            {
+                playerRigidBody.transform.DOScaleX(-fallScaleX, durationAnimFall).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+                playerRigidBody.transform.DOScaleY(fallScaleY, durationAnimFall).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+            }
             _Anim = true;
         }
+    }
+
+    private void Init()
+    {
+        _Anim = false;
+        _jumping = false;
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
         _jumping = true;
+        animatorPlayer.SetBool(landingBool, false);
     }
 
     void Update()
@@ -99,7 +157,6 @@ public class PlayerController : MonoBehaviour
 
     private void Awake()
     {
-        _Anim = false;
-        _jumping = false;
+        
     }
 }
