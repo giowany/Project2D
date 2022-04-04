fad4974ff5c9a7b665a5d53e91a6e7cefd941770
Giowany Ribeiro <giowany_100@hotmail.com>
2022-04-04 19:42:40 -0300

    Update ItemManager.cs

--- Assets/Scripts/Managers/ItemManager.cs
+++ Assets/Scripts/Managers/ItemManager.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using EBAC.Core.Singleton;
 
 public class ItemManager : Singleton<ItemManager> 
 {
     [SerializeField] private int _coins;
-
+    public TextMeshProUGUI textCoins;
     public void AddCoins(int amount = 1)
     {
         _coins += amount;
+        textCoins.text = "X" + _coins.ToString();
     }
 
     private void ResetItens()
