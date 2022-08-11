86122de030840c432c3670be1c83d7822696b4e9
Giowany Ribeiro <giowany_100@hotmail.com>
2022-08-11 19:32:31 -0300

    Update HealthBase.cs

--- Assets/Scripts/Health/HealthBase.cs
+++ Assets/Scripts/Health/HealthBase.cs
@@ -6,26 +6,44 @@ public class HealthBase : MonoBehaviour
 {
     [Header("References")]
     public Animator playeranimator;
+    public FlashDamage flashDamage;
 
     public int startLife = 10;
     public float timeToDie = 1f;
     public string healthFloat = "Health";
     public string deadBool = "Dead";
+    public float invensibleTime = 1f;
 
     private int _currenteLife;
     private bool _isDead = false;
+    private bool _isInvencible = false;
 
     public void Damage(int damage)
     {
         if (_isDead) return;
+        if (_isInvencible) return;
 
         _currenteLife -= damage;
 
         if (_currenteLife <= 0)
         {
+            var a = GetComponent<PlayerController>();
+            if (a != null) a.GetComponent<PlayerController>().Dead(true);
             Kill();
             Invoke(nameof(SetDead), .1f);
         }
+
+        else if (_currenteLife >= 5)
+        {
+            flashDamage.Flash();
+            _isInvencible = true;
+            Invoke(nameof(InvensibleOff), invensibleTime);
+        }
+    }
+
+    private void InvensibleOff()
+    {
+        _isInvencible = false;
     }
 
     private void SetDead()
